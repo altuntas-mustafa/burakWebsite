70aa155427b490f1b4916442930fa1c48a18fd25
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-08 15:10:02 +0200

    footer complated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram } from "@mui/icons-material";
+import { Box } from "@mui/material";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <div>Footeer</div>
-  )
+    <Box
+    component="footer"
+    sx={{
+      backgroundColor: (theme) =>
+        theme.palette.mode === "light"
+          ? theme.palette.grey[200]
+          : theme.palette.grey[800],
+      p: 6,
+      position: "fixed", // Set the position to "fixed"
+      left: 0,
+      bottom: 0,
+      width: "100%",
+    }}
+  >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              About Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              We are XYZ company, dedicated to providing the best service to our
+              customers.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              123 Main Street, Anytown, USA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: info@example.com
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +1 234 567 8901
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/people/Anatolie-Gordijn-Breda/100093649630304/" target="_blank" color="inherit">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/anatoliegordijn/?igshid=MjEwN2IyYWYwYw%3D%3D"
+              color="inherit"
+              target="_blank"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © Burak Website  "}
+            
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
-
-export default Footer
